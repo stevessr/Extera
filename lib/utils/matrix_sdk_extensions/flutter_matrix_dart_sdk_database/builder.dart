a626b50b285c7b0e6360a1e674d59dfac4a9d31d
ryotairi <ryotairi.383e@extera.xyz>
2026-07-29 18:42:01 +0500

    chore: use sqlite3

--- lib/utils/matrix_sdk_extensions/flutter_matrix_dart_sdk_database/builder.dart
+++ lib/utils/matrix_sdk_extensions/flutter_matrix_dart_sdk_database/builder.dart
@@ -1,60 +1,49 @@
+// SPDX-FileCopyrightText: 2019-Present Christian Kußowski
+// SPDX-FileCopyrightText: 2019-Present Contributors to FluffyChat
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 import 'dart:io';
 
+import 'package:extera_next/generated/l10n/l10n.dart';
+import 'package:extera_next/utils/client_manager.dart';
+import 'package:extera_next/utils/platform_infos.dart';
 import 'package:flutter/foundation.dart';
-
 import 'package:matrix/matrix.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:universal_html/html.dart' as html;
 
-import 'package:extera_next/config/app_config.dart';
-import 'package:extera_next/generated/l10n/l10n.dart';
-import 'package:extera_next/utils/client_manager.dart';
-import 'package:extera_next/utils/platform_infos.dart';
 import 'cipher.dart';
 
-import 'sqlcipher_stub.dart'
-    if (dart.library.io) 'package:sqlcipher_flutter_libs/sqlcipher_flutter_libs.dart';
-
 Future<DatabaseApi> flutterMatrixSdkDatabaseBuilder(String clientName) async {
-  MatrixSdkDatabase? database;
   try {
-    database = await _constructDatabase(clientName);
-    await database.open();
-    return database;
+    return await _constructDatabase(clientName);
   } catch (e, s) {
     Logs().wtf('Unable to construct database!', e, s);
-    // Try to delete database so that it can created again on next init:
-    database?.delete().catchError(
-      (e, s) => Logs().wtf(
-        'Unable to delete database, after failed construction',
-        e,
-        s,
-      ),
-    );
-
-    // Delete database file:
-    if (database == null && !kIsWeb) {
-      final dbFile = File(await _getDatabasePath(clientName));
-      if (await dbFile.exists()) await dbFile.delete();
-    }
 
     try {
       // Send error notification:
       final l10n = await lookupL10n(PlatformDispatcher.instance.locale);
-      ClientManager.sendInitNotification(
-        l10n.initAppError,
-        l10n.databaseBuildErrorBody(
-          AppConfig.newIssueUrl.toString(),
-          e.toString(),
-        ),
-      );
+      // We expect that the database cannot be open on iOS 2.8.0 due to that
+      // the team ID has changed and te app can no longer access the database
+      // key in the iOS keychain. This should be removed from 2.9.0 on.
+      if (!PlatformInfos.isIOS) {
+        ClientManager.sendInitNotification(l10n.initAppError, e.toString());
+      }
     } catch (e, s) {
       Logs().e('Unable to send error notification', e, s);
     }
 
-    rethrow;
+    // Delete database file:
+    if (!kIsWeb) {
+      final dbFile = File(await _getDatabasePath(clientName));
+      if (await dbFile.exists()) await dbFile.delete();
+    }
+
+    // Try again
+    return await _constructDatabase(clientName);
   }
 }
 
@@ -68,12 +57,12 @@ Future<MatrixSdkDatabase> _constructDatabase(String clientName) async {
 
   Directory? fileStorageLocation;
   try {
-    final tmpDir = await getTemporaryDirectory();
-    final appTmpDir = Directory(join(tmpDir.path, clientName));
-    if (!await appTmpDir.exists()) {
-      await appTmpDir.create(recursive: true);
-    }
-    fileStorageLocation = appTmpDir;
+    final temporaryDirectory = PlatformInfos.isLinux
+        ? await getApplicationCacheDirectory()
+        : await getTemporaryDirectory();
+    fileStorageLocation = await Directory(
+      join(temporaryDirectory.path, 'fluffychat_download_cache'),
+    ).create(recursive: true);
   } on MissingPlatformDirectoryException catch (_) {
     Logs().w(
       'No temporary directory for file cache available on this platform.',
@@ -82,18 +71,14 @@ Future<MatrixSdkDatabase> _constructDatabase(String clientName) async {
 
   final path = await _getDatabasePath(clientName);
 
-  // fix dlopen for old Android
-  await applyWorkaroundToOpenSqlCipherOnOldAndroidVersions();
   // import the SQLite / SQLCipher shared objects / dynamic libraries
-  final factory = createDatabaseFactoryFfi(
-    ffiInit: SQfLiteEncryptionHelper.ffiInit,
-  );
-
-  // migrate from potential previous SQLite database path to current one
-  await _migrateLegacyLocation(path, clientName);
+  final factory = createDatabaseFactoryFfi();
 
   // required for [getDatabasesPath]
   databaseFactory = factory;
+
+  // migrate from potential previous SQLite database path to current one
+  await _migrateLegacyLocation(path, clientName);
 
   // in case we got a cipher, we use the encryption helper
   // to manage SQLite encryption
