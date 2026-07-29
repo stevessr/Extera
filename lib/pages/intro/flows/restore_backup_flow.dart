52817611c0aa5ca96b74cb25a8341eb3597a3c11
ryotairi <ryotairi.383e@extera.xyz>
2026-07-29 19:02:40 +0500

    feat: go to /rooms after restoring session

--- lib/pages/intro/flows/restore_backup_flow.dart
+++ lib/pages/intro/flows/restore_backup_flow.dart
@@ -3,19 +3,28 @@ import 'package:flutter/material.dart';
 import 'package:extera_next/utils/file_selector.dart';
 import 'package:extera_next/widgets/future_loading_dialog.dart';
 import 'package:extera_next/widgets/matrix.dart';
+import 'package:go_router/go_router.dart';
 
 Future<void> restoreBackupFlow(BuildContext context) async {
+  final mx = Matrix.of(context);
+
   final picked = await selectFiles(context);
   final file = picked.firstOrNull;
   if (file == null) return;
 
   if (!context.mounted) return;
-  await showFutureLoadingDialog(
+
+  final result = await showFutureLoadingDialog<bool>(
     context: context,
     future: () async {
-      final client = await Matrix.of(context).getLoginClient();
+      final client = await mx.getLoginClient();
       await client.importDump(String.fromCharCodes(await file.readAsBytes()));
-      Matrix.of(context).initMatrix();
+      mx.initMatrix();
+      return client.isLogged();
     },
   );
+
+  if (result.result == true && context.mounted) {
+    context.go('/rooms');
+  }
 }
