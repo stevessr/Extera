032a6c8f7799a6f69f2a90e628eb33b1546fdb3f
rustyraven <rustyraven@extera.xyz>
2026-08-01 20:00:15 +0500

    chore: update workflow

--- scripts/prepare-android-release.sh
+++ scripts/prepare-android-release.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 cd android
-echo $FDROID_KEY | base64 --decode --ignore-garbage > key.jks
-echo "storePassword=${FDROID_KEY_PASS}" >> key.properties
-echo "keyPassword=${FDROID_KEY_PASS}" >> key.properties
-echo "keyAlias=key" >> key.properties
+echo $KEYSTORE_FILE | base64 --decode --ignore-garbage > key.jks
+echo "storePassword=${KEYSTORE_PASS}" >> key.properties
+echo "keyPassword=${KEY_PASS}" >> key.properties
+echo "keyAlias=${KEY_ALIAS}" >> key.properties
 echo "storeFile=../key.jks" >> key.properties
-echo $PLAYSTORE_DEPLOY_KEY >> keys.json
+#echo $PLAYSTORE_DEPLOY_KEY >> keys.json
 ls | grep key
-bundle install
-bundle update fastlane
-bundle exec fastlane set_build_code_internal
+#bundle install
+#bundle update fastlane
+#bundle exec fastlane set_build_code_internal
+# We don't have google play releases yet, so it's safe to comment this out
 cd ..
