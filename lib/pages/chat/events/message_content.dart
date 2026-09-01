2144e7b417868feb14438a8923d808cf13071696
stevessr <2984716340@qq.com>
2026-09-01 18:46:07 +0800

    fix: unify plain and rich message font resolution

--- lib/pages/chat/events/message_content.dart
+++ lib/pages/chat/events/message_content.dart
@@ -17,6 +17,7 @@ import 'package:extera_next/pages/chat/events/video_player.dart';
 import 'package:extera_next/utils/adaptive_bottom_sheet.dart';
 import 'package:extera_next/utils/animated_emoji.dart';
 import 'package:extera_next/utils/date_time_extension.dart';
+import 'package:extera_next/utils/font_family.dart';
 import 'package:extera_next/utils/matrix_sdk_extensions/matrix_locals.dart';
 import 'package:extera_next/utils/poll_events.dart';
 import 'package:extera_next/widgets/avatar.dart';
@@ -141,6 +142,15 @@ class MessageContent extends StatelessWidget {
   Widget build(BuildContext context) {
     final fontSize =
         AppSettings.fontSizeFactor.value * AppSettings.messageFontSize.value;
+    final fontFamily = resolveFontFamily(
+      useSystemFont: AppSettings.systemFont.value,
+      configuredFont: AppSettings.chatFont.value,
+    );
+    final fontFamilyFallback = resolveFontFallbacks(
+      configuredFallbacks: AppSettings.chatFallbackFonts.value,
+      primaryFont: fontFamily,
+      includeNotoEmoji: AppSettings.notoEmojiFont.value,
+    );
     final buttonTextColor = textColor;
     final bigEmotes =
         event.onlyEmotes && event.numberEmotes > 0 && event.numberEmotes <= 3;
@@ -319,19 +329,8 @@ class MessageContent extends StatelessWidget {
                         AppSettings.fontSizeFactor.value *
                         AppSettings.messageFontSize.value,
                     decoration: .none,
-                    fontFamily: AppSettings.systemFont.value
-                        ? 'SystemFont'
-                        : AppSettings.chatFont.value.isNotEmpty
-                        ? AppSettings.systemFont.value
-                              ? 'SystemFont'
-                              : AppSettings.chatFont.value
-                        : null,
-                    fontFamilyFallback: AppSettings.notoEmojiFont.value
-                        ? [
-                            'Noto Color Emoji',
-                            ...AppSettings.chatFallbackFonts.value.split(','),
-                          ]
-                        : AppSettings.chatFallbackFonts.value.split(','),
+                    fontFamily: fontFamily,
+                    fontFamilyFallback: fontFamilyFallback,
                   ),
                   onOpen: (url) => UrlLauncher(context, url.url).launchUrl(),
                   onCopy: () {
@@ -411,37 +410,15 @@ class MessageContent extends StatelessWidget {
               color: textColor,
               fontSize: bigEmotes ? fontSize * 2 : fontSize,
               decoration: event.redacted ? TextDecoration.lineThrough : null,
-              fontFamily: AppSettings.systemFont.value
-                  ? 'SystemFont'
-                  : AppSettings.chatFont.value.isNotEmpty
-                  ? AppSettings.systemFont.value
-                        ? 'SystemFont'
-                        : AppSettings.chatFont.value
-                  : null,
-              fontFamilyFallback: AppSettings.notoEmojiFont.value
-                  ? [
-                      'Noto Color Emoji',
-                      ...AppSettings.chatFallbackFonts.value.split(','),
-                    ]
-                  : AppSettings.chatFallbackFonts.value.split(','),
+              fontFamily: fontFamily,
+              fontFamilyFallback: fontFamilyFallback,
             );
             final messageLinkStyle = TextStyle(
               color: linkColor,
               fontSize: fontSize,
               decoration: .none,
-              fontFamily: AppSettings.systemFont.value
-                  ? 'SystemFont'
-                  : AppSettings.chatFont.value.isNotEmpty
-                  ? AppSettings.systemFont.value
-                        ? 'SystemFont'
-                        : AppSettings.chatFont.value
-                  : null,
-              fontFamilyFallback: AppSettings.notoEmojiFont.value
-                  ? [
-                      'Noto Color Emoji',
-                      ...AppSettings.chatFallbackFonts.value.split(','),
-                    ]
-                  : AppSettings.chatFallbackFonts.value.split(','),
+              fontFamily: fontFamily,
+              fontFamilyFallback: fontFamilyFallback,
             );
             final spanChildren = <InlineSpan>[
               ...replaceEmojiInSpans(
