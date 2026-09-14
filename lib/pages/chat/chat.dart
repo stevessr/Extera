7088a9b085d8c6e5a4fb6ae517459449e250abc0
meow <meow@localhost>
2026-09-14 21:41:10 +1200

    Make sure autofocus is only happening once in chat page

--- lib/pages/chat/chat.dart
+++ lib/pages/chat/chat.dart
@@ -156,6 +156,8 @@ class ChatController extends State<ChatPageWithRoom>
   bool currentlyTyping = false;
   bool dragging = false;
 
+  bool focusWasRequested = false;
+
   List<String> eventsToScrollBackTo = [];
 
   void onDragEntered(_) => setState(() => dragging = true);
@@ -2326,6 +2328,14 @@ class ChatController extends State<ChatPageWithRoom>
 
   @override
   Widget build(BuildContext context) {
+    if (!focusWasRequested && !PlatformInfos.isMobile){
+      focusWasRequested=true;
+      WidgetsBinding.instance.addPostFrameCallback((_){
+        if (!mounted) return;
+        inputFocus.requestFocus();
+      });
+    }
+
     final theme = Theme.of(context);
     return Row(
       children: [
--- lib/pages/chat/input_bar.dart
+++ lib/pages/chat/input_bar.dart
@@ -38,7 +38,7 @@ class InputBar extends StatelessWidget {
   final TextEditingController? controller;
   final InputDecoration decoration;
   final ValueChanged<String>? onChanged;
-  final bool? autofocus;
+  //final bool? autofocus;
   final bool readOnly;
 
   static List<_CachedEmoji>? _cachedEmojis;
@@ -56,7 +56,7 @@ class InputBar extends StatelessWidget {
     this.controller,
     required this.decoration,
     this.onChanged,
-    this.autofocus,
+    //this.autofocus,
     this.textInputAction,
     this.readOnly = false,
     super.key,
@@ -452,7 +452,7 @@ class InputBar extends StatelessWidget {
         maxLines: maxLines,
         keyboardType: keyboardType,
         textInputAction: textInputAction,
-        autofocus: false,
+        //autofocus: false,
         spellCheckConfiguration: PlatformInfos.supportsSpellCheck
             ? const SpellCheckConfiguration()
             : null,
