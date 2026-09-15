10939a36d3e123f0a2b79a6dfd31655e022e9806
rustyraven <rustyraven@extera.xyz>
2026-09-15 18:29:21 +0500

    fix: text input dialog message style

--- lib/widgets/adaptive_dialogs/show_text_input_dialog.dart
+++ lib/widgets/adaptive_dialogs/show_text_input_dialog.dart
@@ -49,7 +49,8 @@ Future<String?> showTextInputDialog({
                   SelectableLinkify(
                     text: message,
                     textScaleFactor: MediaQuery.textScalerOf(context).scale(1),
-                    linkStyle: TextStyle(
+                    style: Theme.of(context).textTheme.bodyMedium,
+                    linkStyle: Theme.of(context).textTheme.bodyMedium?.copyWith(
                       color: Theme.of(context).colorScheme.primary,
                       decorationColor: Theme.of(context).colorScheme.primary,
                     ),
