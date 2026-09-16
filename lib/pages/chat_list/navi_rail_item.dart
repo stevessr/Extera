775ac72e79707fa3789cac2c7f59bc5f3ada11cd
stevessr <2984716340@qq.com>
2026-09-16 20:31:55 +0800

    perf: remove navigation rail motion in power saver

--- lib/pages/chat_list/navi_rail_item.dart
+++ lib/pages/chat_list/navi_rail_item.dart
@@ -51,7 +51,7 @@ class NaviRailItem extends StatelessWidget {
                 left: 0,
                 child: AnimatedContainer(
                   width: isSelected ? 8 : 0,
-                  duration: FluffyThemes.animationDuration,
+                  duration: FluffyThemes.effectiveAnimationDuration,
                   curve: FluffyThemes.animationCurve,
                   decoration: BoxDecoration(
                     color: theme.colorScheme.primary,
@@ -65,7 +65,7 @@ class NaviRailItem extends StatelessWidget {
               Center(
                 child: AnimatedScale(
                   scale: hovered ? 1.1 : 1.0,
-                  duration: FluffyThemes.animationDuration,
+                  duration: FluffyThemes.effectiveAnimationDuration,
                   curve: FluffyThemes.animationCurve,
                   child: Material(
                     borderRadius: borderRadius,
