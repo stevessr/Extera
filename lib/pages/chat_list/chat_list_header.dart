f20ab0458e0d55d69cbcf653a2dd0268c9360b34
rustyraven <rustyraven@extera.xyz>
2026-09-18 00:02:49 +0500

    fix: incorrect progress indicator shape in chat list header

--- lib/pages/chat_list/chat_list_header.dart
+++ lib/pages/chat_list/chat_list_header.dart
@@ -213,24 +213,24 @@ class _ChatListHeaderDelegate extends SliverPersistentHeaderDelegate {
                                             ),
                                           )
                                   : Container(
-                                      margin: const EdgeInsets.all(8),
-                                      width: 8,
-                                      height: 8,
-                                      child: Center(
-                                        child:
-                                            CircularProgressIndicator.adaptive(
-                                              constraints: const .tightFor(
-                                                width: 24,
-                                                height: 32,
-                                              ),
-                                              strokeWidth: 2,
-                                              value: status.progress,
-                                              valueColor: status.error != null
-                                                  ? AlwaysStoppedAnimation<
-                                                      Color
-                                                    >(theme.colorScheme.error)
-                                                  : null,
+                                      margin: const EdgeInsets.all(2),
+                                      child: SizedBox.square(
+                                        dimension: 24,
+                                        child: Center(
+                                          child: CircularProgressIndicator(
+                                            constraints: const .tightFor(
+                                              width: 24,
+                                              height: 24,
                                             ),
+                                            strokeWidth: 2,
+                                            value: status.progress,
+                                            valueColor: status.error != null
+                                                ? AlwaysStoppedAnimation<Color>(
+                                                    theme.colorScheme.error,
+                                                  )
+                                                : null,
+                                          ),
+                                        ),
                                       ),
                                     ),
                               suffixIcon:
